acfd675e3f385b3f5b2736ac1c4512da953ec4cd
ishikaag <ishikaag@buffalo.edu>
2021-04-08 10:18:11 -0500

    commit

--- src/main/java/PrintingMultiplicationTable.java
+++ src/main/java/PrintingMultiplicationTable.java
@@ -5,13 +5,14 @@ public class PrintingMultiplicationTable {
         String table="";
         while(i<=10){
             mul=i*n;
-            table= n+" X "+i+" = "+mul;
+            table+= n+" X "+i+" = "+mul+"\n" ;
             i++;
         }
         return table;
     }
     public static void main (String args[]){
         int n=9;
-        System.out.println(getTable(n));
+        String table=getTable(n);
+        System.out.println(table);
     }
 }
