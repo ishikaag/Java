140f7e3402aaca8ae9a13e2e5e144235b527335c
ishikaag <ishikaag@buffalo.edu>
2021-04-11 22:55:47 -0500

    commit

--- src/main/java/PrimeNumber.java
+++ src/main/java/PrimeNumber.java
@@ -1,15 +1,20 @@
 public class PrimeNumber {
     public static boolean checkPrime(int n){
         boolean flag=true;
+        if(n<=1){
+            flag=false;
+        }
         for(int i=2; i<=n/2; i++){
             if(n%i==0){
-                flag=false;
+                flag= false;
             }
+            else
+                flag= true;
         }
         return flag;
     }
     public static void main(String args[]){
-        int n=7;
+        int n=1;
         boolean res=checkPrime(n);
         System.out.println( n+ " is prime number: "+res);
     }
